da2519267e400bed7cad186547f2a7d9e4b54ca5
samreensaif <samreensaif81@gmail.com>
2025-12-26 20:04:35 +0500

    Fix: load env variables automatically using python-dotenv

--- api/todo/main.py
+++ api/todo/main.py
@@ -1,5 +1,9 @@
 """FastAPI app for Todo API (Phase II)."""
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
